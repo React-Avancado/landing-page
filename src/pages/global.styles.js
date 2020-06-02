c9524a8a76d48a5b51ac8ad8e3f78846a609736b
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-06-02 17:47:20 -0300

    Add styled-reset

--- src/pages/global.styles.js
+++ src/pages/global.styles.js
@@ -1,19 +1,12 @@
 import { createGlobalStyle } from 'styled-components'
+import reset from 'styled-reset'
 
 const GlobalStyle = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-  }
+  ${reset}
 
-  html, body, #__next {
-    height: 100%;
-  }
-
-  body {
-    font-family: -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    margin: 0;
-    padding: 0;
+  html {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 62.5%;
   }
 `
 
