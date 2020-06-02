87693c79cda4ae689f45b84ae96db8d3b77a6e82
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-06-02 14:35:44 -0300

    Change Main to use a Single Image and remove unused stuff

--- src/components/Main/styles.js
+++ src/components/Main/styles.js
@@ -6,26 +6,9 @@ export const Container = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color: #444;
-  color: #fff;
-`
+  background-color: #06092b;
 
-export const Logo = styled.article`
-  margin: 10px;
-  text-align: center;
-`
-
-export const Lib = styled.span`
-  margin-right: 5px;
-  font-size: 14px;
-  font-weight: bold;
-`
-
-export const Version = styled.span`
-  font-size: 10px;
-  color: #dbdbdb;
-`
-
-export const Title = styled.h1`
-  text-align: center;
+  img {
+    max-width: 100%;
+  }
 `
