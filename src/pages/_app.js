88e671012ee77f0f4d0274a9c2faf2fe3fdd4455
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-06-02 15:13:26 -0300

    Add analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,70 +1,78 @@
-import React from 'react'
-import NextApp from 'next/app'
+import React, { useEffect } from 'react'
+import Router from 'next/router'
 import Head from 'next/head'
 import GlobalStyle from './global.styles'
 
-export default class App extends NextApp {
-  render() {
-    const { Component, pageProps } = this.props
+import { pageview } from 'utils/ga'
 
-    return (
-      <>
-        <Head>
-          <title>
-            React Avançado - Crie aplicações reais com NextJS, GraphQL e mais.
-          </title>
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="theme-color" content="#06092B" />
-          <link rel="shortcut icon" href="/img/icon-512.png" />
-          <link rel="apple-touch-icon" href="/img/icon-512.png" />
-          <link rel="canonical" href="https://reactavancado.com.br/" />
-          <meta
-            name="title"
-            content="React Avançado - Crie aplicações reais com NextJS, GraphQL e mais."
-          />
-          <meta
-            name="description"
-            content="Aprenda a criar um Ecommerce completo, indo do Backend ao Frontend utilizando tecnologias como ReactJS, NextJS, Strapi, GraphQL, Apollo e muito mais!"
-          />
+const App = ({ Component, pageProps }) => {
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url)
+    }
 
-          {/* Facebook. Linkedin, others */}
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://reactavancado.com.br/" />
-          <meta
-            property="og:title"
-            content="React Avançado - Crie aplicações reais com NextJS, GraphQL e mais."
-          />
-          <meta
-            property="og:description"
-            content="Aprenda a criar um Ecommerce completo, indo do Backend ao Frontend utilizando tecnologias como ReactJS, NextJS, Strapi, GraphQL, Apollo e muito mais!"
-          />
-          <meta
-            property="og:image"
-            content="https://reactavancado.com.br/img/cover.png"
-          />
+    Router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
 
-          {/* Twitter */}
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta
-            property="twitter:url"
-            content="https://reactavancado.com.br/"
-          />
-          <meta
-            property="twitter:title"
-            content="React Avançado - Crie aplicações reais com NextJS, GraphQL e mais."
-          />
-          <meta
-            property="twitter:description"
-            content="Aprenda a criar um Ecommerce completo, indo do Backend ao Frontend utilizando tecnologias como ReactJS, NextJS, Strapi, GraphQL, Apollo e muito mais!"
-          />
-          <meta
-            property="twitter:image"
-            content="https://reactavancado.com.br/img/cover.png"
-          />
-        </Head>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </>
-    )
-  }
+  return (
+    <>
+      <Head>
+        <title>
+          React Avançado - Crie aplicações reais com NextJS, GraphQL e mais.
+        </title>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="canonical" href="https://reactavancado.com.br/" />
+        <meta
+          name="title"
+          content="React Avançado - Crie aplicações reais com NextJS, GraphQL e mais."
+        />
+        <meta
+          name="description"
+          content="Aprenda a criar um Ecommerce completo, indo do Backend ao Frontend utilizando tecnologias como ReactJS, NextJS, Strapi, GraphQL, Apollo e muito mais!"
+        />
+
+        {/* Facebook. Linkedin, others */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://reactavancado.com.br/" />
+        <meta
+          property="og:title"
+          content="React Avançado - Crie aplicações reais com NextJS, GraphQL e mais."
+        />
+        <meta
+          property="og:description"
+          content="Aprenda a criar um Ecommerce completo, indo do Backend ao Frontend utilizando tecnologias como ReactJS, NextJS, Strapi, GraphQL, Apollo e muito mais!"
+        />
+        <meta
+          property="og:image"
+          content="https://reactavancado.com.br/img/cover.png"
+        />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://reactavancado.com.br/" />
+        <meta
+          property="twitter:title"
+          content="React Avançado - Crie aplicações reais com NextJS, GraphQL e mais."
+        />
+        <meta
+          property="twitter:description"
+          content="Aprenda a criar um Ecommerce completo, indo do Backend ao Frontend utilizando tecnologias como ReactJS, NextJS, Strapi, GraphQL, Apollo e muito mais!"
+        />
+        <meta
+          property="twitter:image"
+          content="https://reactavancado.com.br/img/cover.png"
+        />
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
+
+export default App
---
+++ src/utils/ga.js
@@ -0,0 +1,15 @@
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url) => {
+  window.gtag('config', process.env.NEXT_PUBLIC_GA_TRACKING, {
+    page_path: url
+  })
+}
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }) => {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value
+  })
+}
