f72878a25786c6f13f115b1d87746066d032759e
Guilherme Louro <guilherme-louro@hotmail.com>
2020-06-06 11:54:40 -0300

    Create ProfileCard

---
+++ src/components/ProfileCard/styles.js
@@ -0,0 +1,62 @@
+import styled, { css } from 'styled-components'
+
+export const Card = styled.article`
+  ${({ theme }) => css`
+    background-color: ${theme.colors.white};
+    padding: ${theme.spacings.medium};
+    text-align: center;
+  `}
+`
+
+export const Image = styled.div`
+  ${({ theme, src }) => css`
+    background: url(${src}) no-repeat center;
+    background-size: cover;
+    width: 12.5rem;
+    height: 12.5rem;
+    border-radius: 100%;
+    margin: auto;
+    margin-bottom: ${theme.spacings.xsmall};
+  `}
+`
+
+export const Name = styled.div`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.medium};
+    font-weight: ${theme.font.bold};
+    color: ${theme.colors.black};
+  `}
+`
+
+export const Role = styled.div`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.small};
+    color: ${theme.colors.primary};
+    margin-bottom: ${theme.spacings.xxsmall};
+  `}
+`
+
+export const SocialLinks = styled.ul`
+  ${({ theme }) => css`
+    display: flex;
+    justify-content: center;
+    margin-bottom: ${theme.spacings.xsmall};
+  `}
+`
+
+export const Link = styled.li`
+  ${({ theme }) => css`
+    margin: 0 0.4rem;
+    display: block;
+    > a {
+      color: ${theme.colors.secondary};
+    }
+  `}
+`
+
+export const Description = styled.div`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.xxsmall};
+    color: ${theme.colors.black};
+  `}
+`
