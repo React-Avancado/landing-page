39d4ab39c80fd03b11320300b3c72c82f77343fa
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-06-06 21:35:26 -0300

    Add slick styles directly

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -64,12 +64,6 @@ const App = ({ Component, pageProps }) => {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
           rel="stylesheet"
         />
-
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-        />
       </Head>
       <GlobalStyle />
       <Component {...pageProps} />
