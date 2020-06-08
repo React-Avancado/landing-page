4ffd4b5c39a1e2ce5d453256096d632642765636
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-06-08 19:02:17 -0300

    Update copy on SectionAgenda

--- src/components/SectionAgenda/index.js
+++ src/components/SectionAgenda/index.js
@@ -11,13 +11,13 @@ const SectionAgenda = () => (
     <S.Text>
       <p>
         Esse é um curso extremamente vivo, estaremos lançando os módulos
-        conforme vamos terminando. Isso além de permitir que vocês iniciem o
-        curso mais rápido, também irá facilitar que tenhamos um feedback mais
+        conforme vamos terminando. Com isso, além de permitir que vocês iniciem
+        o curso mais rápido, também irá facilitar que tenhamos um feedback mais
         constante sobre tudo, deixando nosso curso ainda melhor.
       </p>
 
       <p>
-        Para agradecer as pessoas que nos apoiaram desde o início, conforme os
+        Para agradecer as pessoas que nos apoiarem desde o início, conforme os
         módulos forem sendo lançados, o desconto vai diminuindo.{' '}
         <strong>
           Ou seja, quanto mais cedo você comprar, mais barato irá pagar
