6c95d5978e87532cc83c2452204304093d98837a
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-07-10 18:40:00 -0300

    Improve types on Logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,16 +1,10 @@
 import React from 'react'
 
+import { LogoProps } from 'types/api'
 import { getImageUrl } from 'utils/getImageUrl'
 import * as S from './styles'
 
-type Props = {
-  logo: {
-    url: string
-    alternativeText: string
-  }
-}
-
-const Logo = ({ logo }: Props) => (
+const Logo = ({ logo }: LogoProps) => (
   <S.LogoWrapper src={getImageUrl(logo.url)} alt={logo.alternativeText} />
 )
 
