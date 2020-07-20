0521a03220957dd7f77507331e19f911fead6759
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-07-20 19:26:36 -0300

    Add getStaticProps to get first data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { GetStaticProps } from 'next'
 
 import SectionHero from 'components/SectionHero'
 import SectionAboutProject from 'components/SectionAboutProject'
@@ -12,6 +13,9 @@ import SectionReviews from 'components/SectionReviews'
 import SectionFaq from 'components/SectionFaq'
 import Footer from 'components/Footer'
 import JsonSchema from 'components/JsonSchema'
+
+import client from 'graphql/client'
+import GET_LANDING_PAGE from 'graphql/queries/getLandingPage'
 
 const Index = () => (
   <>
@@ -29,5 +33,17 @@ const Index = () => (
     <JsonSchema />
   </>
 )
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { landingPage } = await client.request(GET_LANDING_PAGE)
+
+  console.log(landingPage)
+
+  return {
+    props: {
+      ...landingPage
+    }
+  }
+}
 
 export default Index
