b67c99543794bfd53042b58d8a6f75814e8e00f8
Guilherme Louro <guilherme-louro@hotmail.com>
2020-07-29 20:05:55 -0300

    remove manifest

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,6 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
