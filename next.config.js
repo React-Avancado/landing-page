583a6365d1e53ad80ee2710a07d0e13d93660971
Guilherme Louro <guilherme-louro@hotmail.com>
2020-07-29 20:02:09 -0300

    remove next-pwa

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
 const withPlugins = require('next-compose-plugins')
-const withPWA = require('next-pwa')
 const optimizedImages = require('next-optimized-images')
 
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = withPlugins([
-  [
-    withPWA,
-    {
-      pwa: {
-        dest: 'public',
-        disable: !isProd
-      }
-    }
-  ],
-  optimizedImages
-])
+module.exports = withPlugins([optimizedImages])
