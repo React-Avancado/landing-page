02b3ff3dfb254a826a33d1011ef3ebcaafda8cf6
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2020-11-06 10:45:20 -0300

    Add BLACKFRIDAY20 coupon

--- src/components/PricingBox/index.tsx
+++ src/components/PricingBox/index.tsx
@@ -37,7 +37,7 @@ const PricingBox = () => (
     </S.BenefitsList>
 
     <Button
-      href="https://www.udemy.com/course/react-avancado/?couponCode=PROMONOV20"
+      href="https://www.udemy.com/course/react-avancado/?couponCode=BLACKFRIDAY20"
       onClick={onClick}
       withPrice
     >
