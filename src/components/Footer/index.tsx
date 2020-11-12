bf6813ac0776e9368fe04031da6a81da8680ca56
Italo <italoaurelior@gmail.com>
2020-11-12 17:24:56 -0300

    Adjust Guilherme Louro Link
    Because him site points to an offline page, I've changed to him github profile.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -10,7 +10,7 @@ const Footer = () => (
       <p>
         Desenvolvido por{' '}
         <a href="https://willianjusten.com.br/">Willian Justen </a>e
-        <a href="https://guilouro.dev"> Guilherme Louro</a>
+        <a href="https://github.com/guilouro"> Guilherme Louro</a>
       </p>
       <p>
         Design por <a href="https://dribbble.com/vmarcosp">Marcos Oliveira</a>
