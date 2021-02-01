b76d4179da8c42d200115ff2adb9edd72ad493b8
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-02-01 14:46:05 -0300

    Add PROMOFEV21 coupon

--- src/components/PricingBox/index.tsx
+++ src/components/PricingBox/index.tsx
@@ -37,7 +37,7 @@ const PricingBox = () => (
     </S.BenefitsList>
 
     <Button
-      href="https://www.udemy.com/course/react-avancado/?couponCode=PROMOJAN21"
+      href="https://www.udemy.com/course/react-avancado/?couponCode=PROMOFEV21"
       onClick={onClick}
       withPrice
     >
