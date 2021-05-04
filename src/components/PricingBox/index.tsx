b8c8a1edff99310f3b675651b45cfa921f79ccc2
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-05-04 11:59:48 -0300

    Update to PROMOMAI21 coupons

--- src/components/PricingBox/index.tsx
+++ src/components/PricingBox/index.tsx
@@ -37,7 +37,7 @@ const PricingBox = () => (
     </S.BenefitsList>
 
     <Button
-      href="https://www.udemy.com/course/react-avancado/?couponCode=PROMOABRIL21"
+      href="https://www.udemy.com/course/react-avancado/?couponCode=PROMOMAI21"
       onClick={onClick}
       withPrice
     >
--- src/components/SectionHero/index.tsx
+++ src/components/SectionHero/index.tsx
@@ -23,7 +23,7 @@ const SectionHero = () => (
           </S.Description>
           <S.ButtonWrapper>
             <Button
-              href="https://www.udemy.com/course/react-avancado/?couponCode=PROMOABRIL21"
+              href="https://www.udemy.com/course/react-avancado/?couponCode=PROMOMAI21"
               onClick={onClick}
               wide
             >
