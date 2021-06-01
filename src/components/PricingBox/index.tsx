cdddf85e3216cd5fc0efa604c06f46bd302c1d4e
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-06-01 11:25:37 -0300

    Add new coupon PROMOJUN21

--- src/components/PricingBox/index.tsx
+++ src/components/PricingBox/index.tsx
@@ -37,7 +37,7 @@ const PricingBox = () => (
     </S.BenefitsList>
 
     <Button
-      href="https://www.udemy.com/course/react-avancado/?couponCode=PROMOMAI21"
+      href="https://www.udemy.com/course/react-avancado/?couponCode=PROMOJUN21"
       onClick={onClick}
       withPrice
     >
--- src/components/SectionHero/index.tsx
+++ src/components/SectionHero/index.tsx
@@ -23,7 +23,7 @@ const SectionHero = () => (
           </S.Description>
           <S.ButtonWrapper>
             <Button
-              href="https://www.udemy.com/course/react-avancado/?couponCode=PROMOMAI21"
+              href="https://www.udemy.com/course/react-avancado/?couponCode=PROMOJUN21"
               onClick={onClick}
               wide
             >
