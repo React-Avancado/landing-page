9ce725b1ae552391bdddb45d47ff205262f8255e
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-09-08 13:20:22 -0300

    Add ULTIMACHANCE coupon

--- src/components/PricingBox/index.tsx
+++ src/components/PricingBox/index.tsx
@@ -15,7 +15,7 @@ const PricingBox = () => (
         De <span>R$549</span> por apenas
       </S.FullPrice>
       <S.DiscountPrice>
-        <span>6x de</span> R$75
+        <span>6x de</span> R$49
       </S.DiscountPrice>
     </S.Prices>
     <S.BenefitsList>
@@ -37,14 +37,14 @@ const PricingBox = () => (
     </S.BenefitsList>
 
     <Button
-      href="https://www.udemy.com/course/react-avancado/?couponCode=PROMOSET21"
+      href="https://www.udemy.com/course/react-avancado/?couponCode=ULTIMACHANCE"
       onClick={onClick}
       withPrice
     >
       <p>Comprar o curso</p>
       <div>
         <S.ButtonFullPrice>R$549</S.ButtonFullPrice>
-        <S.ButtonDiscountPrice>R$449</S.ButtonDiscountPrice>
+        <S.ButtonDiscountPrice>R$299</S.ButtonDiscountPrice>
       </div>
     </Button>
   </S.Box>
