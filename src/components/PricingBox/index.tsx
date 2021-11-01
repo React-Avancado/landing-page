be1cebdfa2d4c8fbf8110ac6553ad3a60ee61dcc
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-11-01 12:55:10 -0300

    Add PROMONOV21 coupon

--- src/components/PricingBox/index.tsx
+++ src/components/PricingBox/index.tsx
@@ -15,7 +15,7 @@ const PricingBox = () => (
         De <span>R$549</span> por apenas
       </S.FullPrice>
       <S.DiscountPrice>
-        <span>6x de</span> R$74
+        <span>6x de</span> R$66
       </S.DiscountPrice>
     </S.Prices>
     <S.BenefitsList>
@@ -37,14 +37,14 @@ const PricingBox = () => (
     </S.BenefitsList>
 
     <Button
-      href="https://www.udemy.com/course/react-avancado/?couponCode=PROMOOUT21"
+      href="https://www.udemy.com/course/react-avancado/?couponCode=PROMONOV21"
       onClick={onClick}
       withPrice
     >
       <p>Comprar o curso</p>
       <div>
         <S.ButtonFullPrice>R$549</S.ButtonFullPrice>
-        <S.ButtonDiscountPrice>R$449</S.ButtonDiscountPrice>
+        <S.ButtonDiscountPrice>R$399</S.ButtonDiscountPrice>
       </div>
     </Button>
   </S.Box>
--- src/components/SectionHero/index.tsx
+++ src/components/SectionHero/index.tsx
@@ -23,7 +23,7 @@ const SectionHero = () => (
           </S.Description>
           <S.ButtonWrapper>
             <Button
-              href="https://www.udemy.com/course/react-avancado/?couponCode=PROMOOUT21"
+              href="https://www.udemy.com/course/react-avancado/?couponCode=PROMONOV21"
               onClick={onClick}
               wide
             >
