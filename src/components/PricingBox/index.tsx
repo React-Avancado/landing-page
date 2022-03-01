986426ae24f7d94668dcd3eba654db2b8475dcd8
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2022-03-01 11:08:38 -0300

    Add PROMOMAR22 coupon

--- src/components/PricingBox/index.tsx
+++ src/components/PricingBox/index.tsx
@@ -37,7 +37,7 @@ const PricingBox = () => (
     </S.BenefitsList>
 
     <Button
-      href="https://www.udemy.com/course/react-avancado/?couponCode=PROMOFEV22"
+      href="https://www.udemy.com/course/react-avancado/?couponCode=PROMOMAR22"
       onClick={onClick}
       withPrice
     >
