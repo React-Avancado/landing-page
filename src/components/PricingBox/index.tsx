ca4661108798d5da91c68ba2df82dd6fe37f1c95
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2022-08-08 17:52:39 -0300

    Update coupons - PROMOAGO22

--- src/components/PricingBox/index.tsx
+++ src/components/PricingBox/index.tsx
@@ -37,7 +37,7 @@ const PricingBox = () => (
     </S.BenefitsList>
 
     <Button
-      href="https://www.udemy.com/course/react-avancado/?couponCode=PROMOJULHO22"
+      href="https://www.udemy.com/course/react-avancado/?couponCode=PROMOAGO22"
       onClick={onClick}
       withPrice
     >
